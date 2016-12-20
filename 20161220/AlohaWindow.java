507f002e137d9e00bbba2c85267f0d95519785ea
lamichhanedilip <diliplami2468@gmail.com>
2016-12-20 11:40:51 +0900

    AlohaWindow.java added.

--- 20161220/AlohaWindow.java
+++ 20161220/AlohaWindow.java
@@ -6,7 +6,22 @@ public class AlohaWindow{
 		
 		JFrame frame = new JFrame();
 
-		frame.setSize(200,200);
+		frame.setSize(500,600);
+
+		frame.setTitle("ディリプ!");
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.setLayout(new FlowLayout());
+      
+		JLabel label = new JLabel("HELLO");
+		Container ctnr = frame.getContentPane();
+		ctnr.add(label);
+
+		JButton button = new JButton("Click Me!");
+		ctnr.add(button);
+
+
 		frame.setVisible(true);
 	}
 }
