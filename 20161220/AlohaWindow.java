3e78a5ec456d376625edf85acda7e806642fce41
lamichhanedilip <diliplami2468@gmail.com>
2016-12-20 10:42:24 +0900

    AlohaWindow

---
+++ 20161220/AlohaWindow.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class AlohaWindow{
+	public static void main(String[] args) {
+		
+		JFrame frame = new JFrame();
+
+		frame.setSize(200,200);
+		frame.setVisible(true);
+	}
+}
