fcfacce13d963037bdf58a8cfcfb360b29273b6d
lamichhanedilip <diliplami2468@gmail.com>
2016-12-22 10:02:37 +0900

    AlohaWindow.java add.

--- 20161222/AlohaWindow.java
+++ 20161222/AlohaWindow.java
@@ -1,5 +1,18 @@
-public class AlohaWindow{
-public static void main(String[] args) {
-		System.out.println("Aloha!");
-	}	
-}
+
+	import java.awt.*;
+	import javax.swing.*;
+
+	public class AlohaWindow {
+		public static void main(String[] args) {
+			JFrame frame;
+
+			frame = new JFrame();
+			frame.setLocation(300,300);
+			frame.setSize(400,400);
+
+
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setVisible(true);
+
+		}
+	}
---
+++ 20161222/MainFrameMan.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+	import javax.swing.*;
+	public class MainFrameMan{
+	public static void main(String[] args) {
+	FrameMan fm = new FrameMan();		
+		}	
+	}
+	class FrameMan {
+		JFrame frame;
+		public FrameMan(){
+			System.out.println("ALOHA2!");
+		}
+	}
