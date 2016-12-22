7712d732c29a4496d24d2f4e35da3351adf60b8d
lamichhanedilip <diliplami2468@gmail.com>
2016-12-22 11:49:13 +0900

    MainFrameMan3.java added.

---
+++ 20161222/MainFrameMan3.java
@@ -0,0 +1,47 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+	public class MainFrameMan3{
+  	public static void main(String[] args){
+		FrameMan fm=new FrameMan();
+		}
+	}
+	class FrameMan implements ActionListener{
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	JButton button;
+	int num=0;
+
+	public FrameMan(){
+	openWindow();
+
+	}
+	private void openWindow(){
+	frame=new JFrame();
+	frame.setLocation(200,200);
+	frame.setSize(1000,1000);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	panel=new JPanel();
+	label=new JLabel("START");
+	button= new JButton("CLICK");
+	button.addActionListener(this);
+
+	panel.add(label);
+	panel.add(button);
+	frame.add (panel);
+	frame.setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e){
+	if(num%2==0){
+	
+	label.setText("FINISH");
+	}else{
+	label.setText("START");
+	}
+	num++;
+}
+}
+
