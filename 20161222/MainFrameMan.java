73cc6f63ccaf457275bca087cc849205689eca45
lamichhanedilip <diliplami2468@gmail.com>
2016-12-22 10:29:27 +0900

    MainFrameMan.java added.

--- 20161222/MainFrameMan.java
+++ 20161222/MainFrameMan.java
@@ -1,13 +1,29 @@
 import java.awt.*;
-	import javax.swing.*;
-	public class MainFrameMan{
-	public static void main(String[] args) {
-	FrameMan fm = new FrameMan();		
-		}	
+import javax.swing.*;
+
+public class MainFrameMan{
+  	public static void main(String[] args){
+		FrameMan fm=new FrameMan();
 	}
-	class FrameMan {
-		JFrame frame;
-		public FrameMan(){
-			System.out.println("ALOHA2!");
-		}
-	}
+}
+	class FrameMan{
+	JFrame frame;
+	JPanel panel;
+	JLabel label;
+	public FrameMan(){
+	System.out.println("aloha");
+	openWindow();
+
+}
+private void openWindow(){
+	frame=new JFrame();
+	frame.setLocation(200,200);
+	frame.setSize(300,300);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	panel=new JPanel();
+	label=new JLabel("Aloha");
+	panel.add(label);
+	frame.add (panel);
+	frame.setVisible(true);
+}
+}
